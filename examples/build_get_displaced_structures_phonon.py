7515be34f09cdd262bd0b9abc34bffb14dcb9403
akash <akashhiregange1999@gmail.com>
2026-02-18 14:11:50 +0000

    Assert statement to check the presence of yaml and geometry files

--- examples/build_get_displaced_structures_phonon.py
+++ examples/build_get_displaced_structures_phonon.py
@@ -9,10 +9,17 @@
 def test_generate_displaced_structures():
     from ase.build import bulk
     import sys
-    from carmm.phonon.pre_process import make_displaced_supercells, get_charges_and_moments, \
-        creating_files_and_directories
+    from carmm.phonon.pre_process import make_displaced_supercells, get_charges_and_moments, creating_files_and_directories
+    import os
     crys = bulk('Al', 'fcc', a=4.121, cubic=True)
 
-    det, supercells = make_displaced_supercells(crys, [1,1,1], 0.01)
-    moments, charges = get_charges_and_moments(det,crys)
-    creating_files_and_directories(supercells, charges, moments)
+    example_path = 'data/phonon_workflow'
+
+    det, supercells = make_displaced_supercells(crys, [1,1,1], 0.01, path=example_path)
+
+    assert (os.path.exists(f'{example_path}/geometry_eq.in'))
+    assert (os.path.exists(f'{example_path}/phonopy_disp.yaml'))
+    # moments, charges = get_charges_and_moments(det,crys)
+    # creating_files_and_directories(supercells, charges, moments)
+
+test_generate_displaced_structures()
