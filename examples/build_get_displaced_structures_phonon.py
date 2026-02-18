62f5351e08bfe3d45cb8bbd5d55b9619c3a67886
akash <akashhiregange1999@gmail.com>
2026-02-18 14:03:07 +0000

    Added the path argument in build_displaced_structure script

--- examples/build_get_displaced_structures_phonon.py
+++ examples/build_get_displaced_structures_phonon.py
@@ -2,15 +2,17 @@
 
 #IMPORTS
 
-from ase.build import bulk
-import sys
-from carmm.phonon.pre_process import make_displaced_supercells, get_charges_and_moments, creating_files_and_directories
+
 # sys.path.append("C:\Users\akash\OneDrive - Cardiff University\Desktop\phonon_workflow")
 # from pre_process import make_displaced_supercells, get_charges_and_moments, creating_files_and_directories
 
+def test_generate_displaced_structures():
+    from ase.build import bulk
+    import sys
+    from carmm.phonon.pre_process import make_displaced_supercells, get_charges_and_moments, \
+        creating_files_and_directories
+    crys = bulk('Al', 'fcc', a=4.121, cubic=True)
 
-crys = bulk('Al', 'fcc', a=4.121, cubic=True)
-
-det, supercells = make_displaced_supercells(crys, [1,1,1], 0.01)
-moments, charges = get_charges_and_moments(det,crys)
-creating_files_and_directories(supercells, charges, moments)
+    det, supercells = make_displaced_supercells(crys, [1,1,1], 0.01)
+    moments, charges = get_charges_and_moments(det,crys)
+    creating_files_and_directories(supercells, charges, moments)
